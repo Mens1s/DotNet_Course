904968ddfdab0ac6551dd27bf15b4fc8b1dfeb2d
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2024-08-06 17:13:40 +0300

    Linq Code Added

---
+++ Generics/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Data;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Ankara", "Diyarbakir", "İstanbul");
+
+            foreach (var item in result)
+            {   
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+    class Product : IEntity
+    {
+        
+    }
+
+    class Customer : IEntity
+    {
+        
+    }
+
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+    
+    interface ICustomerDal : IRepository<Customer>
+    {
+       
+    }
+
+    interface IEntity
+    {
+        
+    }
+    interface IRepository<T> where T:class, IEntity, new() // reference type .. also it has to newable
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+    
+    class ProductDal : IProductDal
+    {
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+        
+    
+}
