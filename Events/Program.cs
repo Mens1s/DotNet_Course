e22766e871e2bd66d1709898b08d625c0a57694a
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2024-08-09 16:26:04 +0300

    Events-Func-Action-Delegates Added

---
+++ Events/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Events
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hardDisk = new Product(50);
+            hardDisk.ProductName = "hard disk";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "iPhone";
+            gsm.StockControlEvent += Gsm_StockControlEvent;           
+            
+            for (int i = 0; i < 10; i++)
+            {
+                hardDisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+        }
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("GSM Amount is too less. Buy new one.");
+        }
+    }
+
+    public delegate void StockControl();
+    
+    public class Product
+    {
+        private int _stock;
+        
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+        
+        public event StockControl StockControlEvent;
+        public String ProductName { get; set; }
+
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+                if (value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            if (Stock < amount)
+            {
+                Console.WriteLine("Stock is empty.");
+                return;
+            }
+            Stock -= amount;
+            Console.WriteLine("Stock amount : {0}", _stock);
+        }
+    }
+}
---
+++ FunctionAndActions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FunctionAndActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Action void functions
+            // Func is a function can be returnable
+            Func<int, int, int> func = Topla;
+            Console.WriteLine(func(3,5));
+
+            Func<int> rand = delegate()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Func<int> rand2 = () => new Random().Next(1, 100);
+            
+            Console.WriteLine(rand());
+            Console.WriteLine(rand2());
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+      
+    }
+}
