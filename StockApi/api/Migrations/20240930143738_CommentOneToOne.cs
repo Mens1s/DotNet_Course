b04404e8f2bb7a46c1a2989948e0aaf20a5affa0
mens1s <ahmet_yigit_02@hotmail.com>
2024-09-30 17:50:43 +0300

    Portfolio Operations Added

---
+++ StockApi/api/Migrations/20240930143738_CommentOneToOne.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "66c5c8ae-7a14-4ace-bd48-4bae8a958b0e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "82686928-59c8-4d02-a9fb-f1aa7cadcd6c");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "428cc374-a7bb-438a-82c6-f261707e37b6", null, "Admin", "ADMIN" },
+                    { "94962482-1d01-4d81-bb08-1fc41f329cc0", null, "User", "USER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "428cc374-a7bb-438a-82c6-f261707e37b6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "94962482-1d01-4d81-bb08-1fc41f329cc0");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "66c5c8ae-7a14-4ace-bd48-4bae8a958b0e", null, "Admin", "ADMIN" },
+                    { "82686928-59c8-4d02-a9fb-f1aa7cadcd6c", null, "User", "USER" }
+                });
+        }
+    }
+}
---
+++ StockApi/api/Models/Stock.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("Stocks")]
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName {get;set;} = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase {get;set;}
+
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal LastDiv {get;set;}
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public DateTime CreatedTime {get;set;} = DateTime.Now;
+
+        public DateTime UpdatedTime {get;set;} = DateTime.Now;
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
+    }
+}
